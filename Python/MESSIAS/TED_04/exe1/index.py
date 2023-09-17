2b077c63b7bd3eb3d0f8dd9d3d035c74eacb3c8c
Gabriel Albanez <gabrielalbanez2701@gmail.com>
2023-09-17 14:22:35 -0300

    TED's Messias

---
+++ Python/MESSIAS/TED_04/exe1/index.py
@@ -0,0 +1,6 @@
+num = float(input('Numero: '))
+
+if num > 0:
+    print("Seu numero é positivo")
+else:
+    print("Seu numero é negativo")
---
+++ Python/MESSIAS/TED_04/exe2/main.py
@@ -0,0 +1,8 @@
+maca = float(input("Maça: "))
+
+if maca > 12:
+    result = maca * 1
+    print(f'Valor total: {result}')
+else:
+    result_else = maca * 1.30
+    print(f"Valor total: {result_else}")
